7b1368049e7ddadd991830d72e64362093884545
Ibrahim Burak Sahin <burakshn96@gmail.com>
2018-06-29 15:21:47 +0300

    When we click at add event button. We now can see a pop up screen.

--- feature/src/main/java/com/example/beyza/calendar_android/feature/MainActivity.java
+++ feature/src/main/java/com/example/beyza/calendar_android/feature/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.beyza.calendar_android.feature;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -13,15 +15,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import java.util.Calendar;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener{
 
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
     CalendarView calendarView;
     TextView selectedDate;
-
+    Button addEvent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +64,26 @@ public class MainActivity extends AppCompatActivity
                 //we need to add here, what the button on the screen will do
             }
         });
+
+        addEvent = (Button) findViewById(R.id.addEventBtn);
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
 
     }
 
